40ecdd84a8a1d7db434916480068f6961819f88e
banastas <hu.bash26@gmail.com>
2025-03-17 16:44:03 +0100

    json

---
+++ JSON/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace JSON;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var guys = new List<Guy>()
+        {
+            new() { Name = "Bob", Clothes = new Outfit() { Top = "t-shirt", Bottom = "jeans" },
+            Hair = new HairStyle() { Color = HairColor.Red, Length = 3.5f }
+            },
+            new() { Name = "Joe", Clothes = new Outfit() { Top = "polo", Bottom = "slacks" },
+            Hair = new HairStyle() { Color = HairColor.Gray, Length = 2.7f }
+            },
+        };
+
+        var options = new JsonSerializerOptions() { WriteIndented = true };
+        var jsonString = JsonSerializer.Serialize(guys, options);
+        Console.WriteLine(jsonString);
+
+        var copy = JsonSerializer.Deserialize<List<Guy>>(jsonString);
+        foreach (var guy in copy)
+            Console.WriteLine("I deserialized this guy: {0}", guy);
+
+
+        Console.WriteLine(JsonSerializer.Serialize(3));
+        Console.WriteLine(JsonSerializer.Serialize((long)-3));
+        Console.WriteLine(JsonSerializer.Serialize((byte)0));
+        Console.WriteLine(JsonSerializer.Serialize(float.MaxValue));
+        Console.WriteLine(JsonSerializer.Serialize(float.MinValue));
+        Console.WriteLine(JsonSerializer.Serialize(true));
+        Console.WriteLine(JsonSerializer.Serialize("Elephant"));
+        Console.WriteLine(JsonSerializer.Serialize("Elephant".ToCharArray()));
+        Console.WriteLine(JsonSerializer.Serialize(" "));
+    }
+}
---
+++ JSON/Style.cs
@@ -0,0 +1,25 @@
+﻿namespace JSON;
+
+class Guy
+{
+    public string Name { get; set; }
+    public HairStyle Hair { get; set; }
+    public Outfit Clothes { get; set; }
+    public override string ToString() => $"{Name} with {Hair} wearing {Clothes}";
+}
+class Outfit
+{
+    public string Top { get; set; }
+    public string Bottom { get; set; }
+    public override string ToString() => $"{Top} and {Bottom}";
+}
+enum HairColor
+{
+    Auburn, Black, Blonde, Blue, Brown, Gray, Platinum, Purple, Red, White
+}
+class HairStyle
+{
+    public HairColor Color { get; set; }
+    public float Length { get; set; }
+    public override string ToString() => $"{Length:0.0} inch {Color} hair";
+}
